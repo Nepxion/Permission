a7254154f59d9672843a04fa33b8911d06a7a6e1
Nepxion <1394997@qq.com>
2018-01-18 12:55:37 +0800

    重构类结构

--- permission-service/src/main/java/com/nepxion/permission/PermissionApplication.java
+++ permission-service/src/main/java/com/nepxion/permission/PermissionApplication.java
@@ -1,8 +1,8 @@
 package com.nepxion.permission;
 
 /**
- * <p>Title: Nepxion Skeleton</p>
- * <p>Description: Nepxion Skeleton For Freemarker</p>
+ * <p>Title: Nepxion Permission</p>
+ * <p>Description: Nepxion Permission</p>
  * <p>Copyright: Copyright (c) 2017-2050</p>
  * <p>Company: Nepxion</p>
  * @author Haojun Ren
@@ -11,10 +11,8 @@ package com.nepxion.permission;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({ com.nepxion.skeleton.framework.config.SkeletonFrameworkConfig.class })
 public class PermissionApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(PermissionApplication.class).web(true).run(args);
