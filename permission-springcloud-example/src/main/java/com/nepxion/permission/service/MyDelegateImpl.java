4d1ed1af80f594ec12c3a3b485d191c4583c608a
Nepxion <1394997@qq.com>
2018-01-18 17:44:53 +0800

    增加注释

--- permission-springcloud-example/src/main/java/com/nepxion/permission/service/MyDelegateImpl.java
+++ permission-springcloud-example/src/main/java/com/nepxion/permission/service/MyDelegateImpl.java
@@ -18,7 +18,7 @@ import com.nepxion.permission.PermissionDelegate;
 import com.nepxion.permission.entity.PermissionEntity;
 import com.nepxion.permission.entity.UserEntity;
 
-// 该接口实现可以跟随业务系统远程调用数据库，也可以通过调用远程独立的权限微服务的API(建议)来实现
+// 该接口实现可以跟随业务系统远程调用数据库，也可以通过调用远程独立的权限微服务的API(建议)来实现(例如通过Feign来做远程调用)
 @Service
 public class MyDelegateImpl implements PermissionDelegate {
     // 权限列表入库
@@ -32,6 +32,10 @@ public class MyDelegateImpl implements PermissionDelegate {
     @Override
     public boolean authorize(String userId, String userType, String permissionName, String permissionType, String serviceName) {
         // 验证用户是否有权限
+        // 需要和用户系统做对接，userId一般为登录名，userType为用户系统类型。目前支持多用户类型，所以通过userType来区分同名登录用户，例如财务系统有用户叫zhangsan，支付系统也有用户叫zhangsan
+        // permissionName即在@Permission注解上定义的name，permissionType为权限类型，目前支持服务级，网关级，界面级三种类型的权限(参考PermissionType.java类的定义)，以支持最大范围的权限微服务系统。服务级的权限即API权限
+        // serviceName即服务名，在application.properties里定义的spring.application.name
+        // 对于验证结果，在后端实现分布式缓存，可以避免频繁调用数据库而出现性能问题
         // 示例描述用户zhangsan有权限，用户lisi没权限
         if (StringUtils.equals(userId, "zhangsan")) {
             return true;
@@ -46,6 +50,7 @@ public class MyDelegateImpl implements PermissionDelegate {
     @Override
     public UserEntity getUserEntity(String token) {
         // 当前端登录后，它希望送token到后端，查询出用户信息(并以此调用authorize接口做权限验证，permission-aop已经实现，使用者并不需要关心)
+        // 需要和单点登录系统，例如OAuth或者JWT等系统做对接
         // 示例描述token为abcd1234对应的用户为lisi
         if (StringUtils.equals(token, "abcd1234")) {
             UserEntity userEntity = new UserEntity();
