9ae0cf4498bc46987475fd4fec422c28248d8f96
Nepxion <1394997@qq.com>
2018-01-18 17:20:36 +0800

    增加注释

--- permission-springcloud-example/src/main/java/com/nepxion/permission/service/MyDelegateImpl.java
+++ permission-springcloud-example/src/main/java/com/nepxion/permission/service/MyDelegateImpl.java
@@ -18,17 +18,21 @@ import com.nepxion.permission.PermissionDelegate;
 import com.nepxion.permission.entity.PermissionEntity;
 import com.nepxion.permission.entity.UserEntity;
 
+// 该接口实现可以跟随业务系统远程调用数据库，也可以通过调用远程独立的权限微服务的API(建议)来实现
 @Service
 public class MyDelegateImpl implements PermissionDelegate {
     // 权限列表入库
     @Override
     public void persist(List<PermissionEntity> permissionEntityList) {
-
+        // 实现权限扫描结果到数据库的入库
+        // 需要注意，权限的重复入库问题，一般遵循“不存在则插入，存在则覆盖”的原则
     }
 
     // 权限验证
     @Override
     public boolean authorize(String userId, String userType, String permissionName, String permissionType, String serviceName) {
+        // 验证用户是否有权限
+        // 示例描述用户zhangsan有权限，用户lisi没权限
         if (StringUtils.equals(userId, "zhangsan")) {
             return true;
         } else if (StringUtils.equals(userId, "lisi")) {
@@ -41,6 +45,8 @@ public class MyDelegateImpl implements PermissionDelegate {
     // 根据Token获取User实体
     @Override
     public UserEntity getUserEntity(String token) {
+        // 当前端登录后，它希望送token到后端，查询出用户信息(并以此调用authorize接口做权限验证，permission-aop已经实现，使用者并不需要关心)
+        // 示例描述token为abcd1234对应的用户为lisi
         if (StringUtils.equals(token, "abcd1234")) {
             UserEntity userEntity = new UserEntity();
             userEntity.setUserId("lisi");
--- permission-springcloud-example/src/main/java/com/nepxion/permission/service/MyService.java
+++ permission-springcloud-example/src/main/java/com/nepxion/permission/service/MyService.java
@@ -15,9 +15,11 @@ import com.nepxion.permission.annotation.UserId;
 import com.nepxion.permission.annotation.UserType;
 
 public interface MyService {
+    // 基于userId和userType的权限验证
     @Permission(name = "A-Permission", label = "A权限")
     int doA(@UserId String userId, @UserType String userType, String value);
 
+    // 基于token的权限验证
     @Permission(name = "B-Permission", label = "B权限", description = "B权限的描述")
     String doB(@Token String token, String value);
 }
