08f6d434088c9cec550ac754b45a89da2c097201
Nepxion <1394997@qq.com>
2018-06-16 22:17:00 +0800

    修复Bug

---
+++ permission-aop/src/main/java/com/nepxion/permission/context/PermissionFeignBeanFactoryPostProcessor.java
@@ -0,0 +1,26 @@
+package com.nepxion.permission.context;
+
+/**
+ * <p>Title: Nepxion Permission</p>
+ * <p>Description: Nepxion Permission</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+// 参考：https://github.com/spring-cloud/spring-cloud-netflix/issues/1952, https://github.com/spring-cloud/spring-cloud-netflix/issues/1064
+@Component
+public class PermissionFeignBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        BeanDefinition definition = beanFactory.getBeanDefinition("feignContext");
+        definition.setDependsOn("eurekaServiceRegistry", "inetUtils");
+    }
+}
