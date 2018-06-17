7475bf2452d982f94875261113f562c628c96ced
Nepxion <1394997@qq.com>
2018-06-17 14:53:41 +0800

    升级版本

--- permission-aop/src/main/java/com/nepxion/permission/aop/PermissionAutoScanProxy.java
+++ permission-aop/src/main/java/com/nepxion/permission/aop/PermissionAutoScanProxy.java
@@ -38,7 +38,7 @@ public class PermissionAutoScanProxy extends DefaultAutoScanProxy {
         System.out.println("║╔══╣║═╣╔╣╚╝╠╣══╣══╬╣╔╗║╔╗║");
         System.out.println("║║  ║║═╣║║║║║╠══╠══║║╚╝║║║║");
         System.out.println("╚╝  ╚══╩╝╚╩╩╩╩══╩══╩╩══╩╝╚╝");
-        System.out.println("Nepxion Permission  v1.0.8");
+        System.out.println("Nepxion Permission  v1.0.9");
         System.out.println("");
     }
 
