d9fc1d525cb145ea81405566389ad0c29649f41e
Nepxion <1394997@qq.com>
2018-07-07 20:19:06 +0800

    升级版本

--- permission-aop/src/main/java/com/nepxion/permission/configuration/PermissionAopConfiguration.java
+++ permission-aop/src/main/java/com/nepxion/permission/configuration/PermissionAopConfiguration.java
@@ -30,7 +30,7 @@ public class PermissionAopConfiguration {
         System.out.println("║╔══╣║═╣╔╣╚╝╠╣══╣══╬╣╔╗║╔╗║");
         System.out.println("║║  ║║═╣║║║║║╠══╠══║║╚╝║║║║");
         System.out.println("╚╝  ╚══╩╝╚╩╩╩╩══╩══╩╩══╩╝╚╝");
-        System.out.println("Nepxion Permission  v1.0.24");
+        System.out.println("Nepxion Permission  v1.0.25");
         System.out.println("");
     }
 
