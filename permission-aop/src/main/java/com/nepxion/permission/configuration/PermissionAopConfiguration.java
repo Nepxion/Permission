b38ef86fe9613d95d2b945b80dc897e09093fc5b
Nepxion <1394997@qq.com>
2018-12-01 22:54:36 +0800

    升级版本到3.0.0

--- permission-aop/src/main/java/com/nepxion/permission/configuration/PermissionAopConfiguration.java
+++ permission-aop/src/main/java/com/nepxion/permission/configuration/PermissionAopConfiguration.java
@@ -30,7 +30,7 @@ public class PermissionAopConfiguration {
         System.out.println("║╔══╣║═╣╔╣╚╝╠╣══╣══╬╣╔╗║╔╗║");
         System.out.println("║║  ║║═╣║║║║║╠══╠══║║╚╝║║║║");
         System.out.println("╚╝  ╚══╩╝╚╩╩╩╩══╩══╩╩══╩╝╚╝");
-        System.out.println("Nepxion Permission  v2.0.8");
+        System.out.println("Nepxion Permission  v3.0.0");
         System.out.println("");
     }
 
--- permission-service/src/main/java/com/nepxion/permission/service/configuration/PermissionServiceConfiguration.java
+++ permission-service/src/main/java/com/nepxion/permission/service/configuration/PermissionServiceConfiguration.java
@@ -27,7 +27,7 @@ public class PermissionServiceConfiguration {
         System.out.println("║╔══╣║═╣╔╣╚╝╠╣══╣══╬╣╔╗║╔╗║");
         System.out.println("║║  ║║═╣║║║║║╠══╠══║║╚╝║║║║");
         System.out.println("╚╝  ╚══╩╝╚╩╩╩╩══╩══╩╩══╩╝╚╝");
-        System.out.println("Nepxion Permission  v2.0.8");
+        System.out.println("Nepxion Permission  v3.0.0");
         System.out.println("");
     }
 
