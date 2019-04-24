9a2cdde5ed831acc0346a0b10c4a6e21cf35c64b
Nepxion <1394997@qq.com>
2019-04-24 14:05:05 +0800

    增加旗标

--- permission-service/src/main/java/com/nepxion/permission/service/configuration/PermissionServiceConfiguration.java
+++ permission-service/src/main/java/com/nepxion/permission/service/configuration/PermissionServiceConfiguration.java
@@ -12,16 +12,22 @@ package com.nepxion.permission.service.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.nepxion.banner.BannerConstant;
+import com.nepxion.banner.Description;
+import com.nepxion.banner.LogoBanner;
+import com.nepxion.banner.NepxionBanner;
 import com.nepxion.permission.api.PermissionResource;
 import com.nepxion.permission.api.UserResource;
+import com.nepxion.permission.service.constant.PermissionServiceConstant;
 import com.nepxion.permission.service.impl.PermissionResourceImpl;
 import com.nepxion.permission.service.impl.UserResourceImpl;
+import com.taobao.text.Color;
 
 @Configuration
 public class PermissionServiceConfiguration {
     static {
-        String logoShown = System.getProperty("nepxion.logo.shown", "true");
-        if (Boolean.valueOf(logoShown)) {
+        /*String bannerShown = System.getProperty(BannerConstant.BANNER_SHOWN, "true");
+        if (Boolean.valueOf(bannerShown)) {
             System.out.println("");
             System.out.println("╔═══╗");
             System.out.println("║╔═╗║");
@@ -29,9 +35,13 @@ public class PermissionServiceConfiguration {
             System.out.println("║╔══╣║═╣╔╣╚╝╠╣══╣══╬╣╔╗║╔╗║");
             System.out.println("║║  ║║═╣║║║║║╠══╠══║║╚╝║║║║");
             System.out.println("╚╝  ╚══╩╝╚╩╩╩╩══╩══╩╩══╩╝╚╝");
-            System.out.println("Nepxion Permission  v2.0.11");
+            System.out.println("Nepxion Permission  v" + PermissionServiceConstant.PERMISSION_VERSION);
             System.out.println("");
-        }
+        }*/
+
+        LogoBanner logoBanner = new LogoBanner(PermissionServiceConfiguration.class, "/com/nepxion/permission/resource/logo.txt", "Welcome to Nepxion", 10, 5, new Color[] { Color.red, Color.green, Color.cyan, Color.blue, Color.yellow, Color.magenta, Color.red, Color.green, Color.cyan, Color.blue }, true);
+
+        NepxionBanner.show(logoBanner, new Description(BannerConstant.VERSION + ":", PermissionServiceConstant.PERMISSION_VERSION, 0, 1), new Description(BannerConstant.GITHUB + ":", BannerConstant.NEPXION_GITHUB + "/Permission", 0, 1));
     }
 
     @Bean
